1826b5c73bb3f7b60be369c8f0a71d22189210de
Nathan Marley <nathan.marley@gmail.com>
2019-11-17 10:33:49 -0300

    Clean up scripts a bit

--- scripts/create_empty_rpc_md.sh
+++ scripts/create_empty_rpc_md.sh
@@ -66,18 +66,3 @@ else
 	echo "Usage: create_empty_rpc_md RPCNAME"
 	echo "Example: create_empty_rpc_md GetBlock"
 fi
-
-#This file is licensed under the MIT License (MIT) available on
-#http://opensource.org/licenses/MIT.
-#{% endcomment %}
-#{% assign filename="_includes/devdoc/dash-core/rpcs/rpcs/getaddressutxos.md" %}#
-#
-###### GetAddressUtxos
-#{% include helpers/subhead-links.md %}
-#
-#{% autocrossref %}
-#
-#{% assign summary_getAddressUtxos="" %}
-#
-#
-#{% endautocrossref %}
--- scripts/generateDocumentationAndDeploy.sh
+++ scripts/generateDocumentationAndDeploy.sh
@@ -1,10 +1,5 @@
-#!/bin/sh
-################################################################################
-# Title         : generateDocumentationAndDeploy.sh
-# Date created  : 2016/02/22
-# Notes         : Modified the thephez for deploying with dash-docs
-__AUTHOR__="Jeroen de Bruijn"
-# Preconditions:
+#! /bin/sh
+#
 # - Doxygen must be installed or loaded from cache. The build-install-doxygen
 #   script should be used to do this
 # - A Doxygen configuration file must be located in the TRAVIS_BUILD_DIR/doxygen
@@ -14,40 +9,20 @@ __AUTHOR__="Jeroen de Bruijn"
 #
 # Required global variables:
 # - DOXYFILE            : The Doxygen configuration file.
-#
-# For information on how to encrypt variables for Travis CI please go to
-# https://docs.travis-ci.com/user/environment-variables/#Encrypted-Variables
-# or https://gist.github.com/vidavidorra/7ed6166a46c537d3cbd2
-# For information on how to create a clean gh-pages branch from the master
-# branch, please go to https://gist.github.com/vidavidorra/846a2fc7dd51f4fe56a0
-#
-# This script will generate Doxygen documentation and copy the documentation to
-# the _site/_en directory of dash-docs so it can be deployed with the rest of
-# the site
-#
-################################################################################
 
-################################################################################
-##### Setup this script and clone the Dash repository                      #####
-echo 'Setting up the script...'
-# Exit with nonzero exit code if anything fails
 set -e
+echo 'Setting up the script...'
 
-# Clone Dash Core
-git clone https://github.com/dashpay/dash.git
+git clone -b develop https://github.com/dashpay/dash.git
 cd dash
 
 # Copy Doxygen config / build files
 cp $TRAVIS_BUILD_DIR/doxygen/* doc/
 
-################################################################################
-##### Generate the Doxygen code documentation and log the output.          #####
 echo 'Generating Doxygen code documentation...'
-# Redirect both stderr and stdout to the log file AND the console.
 $TRAVIS_BUILD_DIR/doxygen-install/doxygen doc/$DOXYFILE 2>&1 | tee doxygen.log
 
 echo 'Done generating Doxygen code documentation...'
-#cat doxygen.log
 
 # Copy Doxygen files to dash-docs folder
 echo 'Copy Doxygen files to _site...'
